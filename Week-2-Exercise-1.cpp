ad018e5313b40aa427054d1b51ad8da923d88f01
ANDERSON MISAEL YOVERA CHINCHAY <u20241f789@upc.edu.pe>
2025-05-19 18:28:11 -0500

    Update Week-2-Exercise-1.cpp

--- Week-2-Exercise-1.cpp
+++ Week-2-Exercise-1.cpp
@@ -1,31 +1,130 @@
 ﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.14.36109.1 d17.14
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Week-2-Exercise-1", "Week-2-Exercise-1.vcxproj", "{7828BCAA-19F0-4672-B122-D60BB0233F90}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Debug|x64.ActiveCfg = Debug|x64
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Debug|x64.Build.0 = Debug|x64
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Debug|x86.ActiveCfg = Debug|Win32
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Debug|x86.Build.0 = Debug|Win32
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Release|x64.ActiveCfg = Release|x64
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Release|x64.Build.0 = Release|x64
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Release|x86.ActiveCfg = Release|Win32
-		{7828BCAA-19F0-4672-B122-D60BB0233F90}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {4A65901F-C2B2-4B1D-BAA7-9DA4F6618C9B}
-	EndGlobalSection
-EndGlobal
+#include "pch.h"
+#include <istream>
+#include <string>
+
+#include "ControladorEclipse.h"
+
+using namespace System;
+using namespace std;
+
+void ventana() {
+    Console::SetWindowSize(72, 40);
+}
+void MostrarMenu(){
+    cout << "\t\t\tECLIPSE\n";
+    cout << "\t=============================================\n";
+    cout << "\t1. Registrar Datos\n";
+    cout << "\t2. Modificar Datos\n";
+    cout << "\t3. Eliminar Datos\n";
+    cout << "\t4. Reporte Eclipses\n";
+    cout << "\t5. Reporte Eclipses Visibles Europa\n";
+    cout << "\t6. Reporte Eclipses Ocasionaron Sismos\n";
+    cout << "\t7. Reporte Eclipses Noche\n";
+    cout << "\t8. Salir\n";
+}
+
+int main()
+{
+    Controlador* controladora = new Controlador();
+    int opcion;
+
+    ventana();
+
+    do
+    {
+        MostrarMenu();
+        cout << "\tIngrese Opcion: "; cin >> opcion;
+        system("cls");
+
+        switch (opcion)
+        {
+        case 1:
+            system("cls");
+            controladora->registrarDatos();
+            cout << "\nSE INGRESO CON EXITO LOS DATOS DEL ECLIPSE!!" << endl;
+            system("pause>0");
+            break;
+        case 2:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->modificarDatos();
+            }
+            system("pause>0");
+            break;
+        case 3:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->eliminarDatos();
+            }
+            system("pause>0");
+            break;
+        case 4:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->reporteTodosEclipses();
+            }
+            system("pause>0");
+            break;
+        case 5:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->reporteElipsesEuropa();
+            }
+            system("pause>0");
+            break;
+        case 6:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->reporteElipsesSismos();
+            }
+            system("pause>0");
+            break;
+        case 7:
+            system("cls");
+            if (!controladora->existenEclipses())
+            {
+                cout << "NO HAY ECLIPSES REGISTRADOS. POR FAVOR REGISTRE LOS DATOS DE UN ECLIPSE." << endl;
+            }
+            else
+            {
+                controladora->reporteElipsesNoche();
+            }
+            system("pause>0");
+            break;
+        case 8:
+            delete controladora;
+            break;
+        default:
+            cout << "Opcion Invalida!" << endl;
+        }
+        system("cls");
+    } while (opcion != 8);
+
+    return 0;
+}
